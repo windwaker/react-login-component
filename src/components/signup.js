13c175f4144efca7f30968dc72b3dfde4fd88416
colm <colm.harrington@gmail.com>
2021-10-18 22:41:35 +0100

    tweak elevation

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,13 +4,13 @@ import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOut
 
 const Signup = () => {
 
-    const paperStyle = { padding: '30px 20px', width: 300, margin: '30px auto' };
+    const paperStyle = { padding: '20px 20px', height: '73vh', width: 280, margin: '0 auto' };
     const headerStyle = { margin: 0 };
     const avatarStyle = { backgroundColor: '#1bbd7e' };
 
     return (
         <Grid>
-            <Paper elevation={20} style={paperStyle}>
+            <Paper style={paperStyle}>
                 <Grid align='center'>
                     <Avatar style={avatarStyle}> <AddCircleOutlineOutlinedIcon /> </Avatar>
                     <h2 style={headerStyle}>Sign Up</h2>
